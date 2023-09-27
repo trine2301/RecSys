47d8700265dd4e98f90cdb61cf24003a2902f327
Trine Staverlokk <trine.merete.staverlokk@conta.no>
2023-09-27 14:13:55 +0200

    Added a simple backend with a helloWorld-controller

---
+++ backend/src/main/java/recsys/App.java
@@ -0,0 +1,10 @@
+package recsys;
+import io.micronaut.runtime.Micronaut;
+
+public class App {
+    public static void main(String[] args) {
+        Micronaut
+                .build(args)
+                .start();
+    }
+}
