3346dcb6e854ea36d6b11d3ab68c4e504c0769a2
Trine Staverlokk <trine.merete.staverlokk@conta.no>
2023-10-23 15:20:14 +0200

    Deleted unessesary entities/classes. Changed project structure. Added testdata

--- frontend/src/views/Accounts.vue
+++ frontend/src/views/Accounts.vue
@@ -2,24 +2,10 @@
   <div>
     Accounts:
   </div>
-    <ul>
-      <li v-for="account in accounts" :key="account.id">
-        {{ account.name }}
-      </li>
-    </ul>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 
-const accounts = ref([]);
-
-async function fetchAccounts() {
-  const response = await fetch('/account');
-  accounts.value = await response.json();
-}
-
-fetchAccounts();
 
 </script>
 
