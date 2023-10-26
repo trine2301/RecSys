5301d4e83a7405062a2f2e08edf8f217fa44b08f
Trine Staverlokk <trine.merete.staverlokk@conta.no>
2023-10-26 15:41:58 +0200

    Fixed small bug in accountingMatcherView

--- frontend/src/views/AccountMatcherView.vue
+++ frontend/src/views/AccountMatcherView.vue
@@ -5,22 +5,9 @@
 </template>
 
 <script setup lang="ts">
-import { inject, ref, provide, onMounted } from "vue"
+
 import PeriodPicker from "@/components/PeriodPicker.vue";
 
-
-const get = inject('get') as Function
-
-const accounts = ref<Array<Account>>([])
-
-const fetchAccounts = async () => {
-  const response = await get('/accounts')
-  accounts.value = response.data
-}
-
-provide('fetchAccounts', fetchAccounts)
-
-onMounted(fetchAccounts)
 </script>
 
 <style lang="scss">
