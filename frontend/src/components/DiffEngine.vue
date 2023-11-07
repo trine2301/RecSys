8a48d1ff7595648f1bd5f82b74466fe202201a93
Trine Staverlokk <trine.merete.staverlokk@conta.no>
2023-11-07 15:14:35 +0100

    Listing bank- and accounting transactions in DiffEngine-component. Toggle button works. Period-picker works.

--- frontend/src/components/DiffEngine.vue
+++ frontend/src/components/DiffEngine.vue
@@ -1,21 +1,35 @@
 
 <template>
-  <div class="bg-blue-200 p-10 h-screen rounded scroll-auto overflow-y-scroll" >
-    <MatchTransactionButton class="my-5" @click="toggleDiffEngine()">
+  <div class=" p-10 h-screen rounded scroll-auto overflow-y-scroll">
+    <period-picker/>
+    <MatchTransactionButton class="my-5" @click="toggleDiffEngineResults()">
       Match transactions
     </MatchTransactionButton>
-    <div class="flex">
-      <div class="bg-blue-100 rounded mx-1 w-80">
-        <div class="text-xl p-2">
-          Accounting transactions
+    <div>
+      <div>
+        <div class="flex" v-if="!isMatchTransactionsButtonPressed">
+          <div class="rounded mx-1">
+            <ListOfTransactions/>
+          </div>
         </div>
-        <ListOfTransactions />
-      </div>
-      <div class="bg-blue-100 rounded h-auto mx-1 w-80">
-        <div class="text-xl p-2">
-          Bank transactions
-        </div >
-        <ListOfTransactions />
+        <div class="bg-blue-100" v-else>
+          <div class="flex">
+            <div>
+              <div class="rounded mx-1">
+                <div class="text-xl p-2">
+                  Accounting transactions
+                </div>
+                Listing accounting transactions with comp.results here
+              </div>
+              <div class="rounded h-auto mx-1">
+                <div class="text-xl p-2">
+                  Bank transactions
+                </div>
+                Listing accounting transactions with comp.results here
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -26,15 +40,24 @@
 
 
 import { ref } from "vue"
-import ListOfTransactions from "@/components/ListOfTransactions.vue";
-import MatchTransactionButton from "@/components/MatchTransactionButton.vue";
+import ListOfTransactions from "@/components/ListOfTransactions.vue"
+import MatchTransactionButton from "@/components/MatchTransactionButton.vue"
 
-const isVisible = ref(false)
+import '@vuepic/vue-datepicker/dist/main.css'
+import PeriodPicker from "@/components/PeriodPicker.vue";
+
+
+const isMatchTransactionsButtonPressed = ref(false)
+const date = ref(new Date())
+const startDate = ref(null)
+const endDate = ref(null)
 
 //  v-if="isVisible"
-const toggleDiffEngine = () => {
-  isVisible.value = !isVisible.value
+const toggleDiffEngineResults = () => {
+  isMatchTransactionsButtonPressed.value = !isMatchTransactionsButtonPressed.value
 }
+
+
 
 </script>
 
